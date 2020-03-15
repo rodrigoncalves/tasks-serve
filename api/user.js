351aa9e5c5eaba29774be274512cc62787f8535f
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2020-03-15 16:54:27 -0300

    103. API: Cadastro de Usuário (Signup)

---
+++ api/user.js
@@ -0,0 +1,23 @@
+const bcrypt = require('bcrypt-nodejs')
+
+module.exports = app => {
+  const getHash = (password, cb) => {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(password, salt, null, (err, hash) => cb(hash))
+    })
+  }
+
+  const save = (req, res) => {
+    getHash(req.body.password, hash => {
+      const password = hash
+
+      app
+        .db('users')
+        .insert({ name: req.body.name, email: req.body.email, password })
+        .then(_ => res.status(204).send())
+        .catch(err => res.status(400).json(err))
+    })
+  }
+
+  return { save }
+}
