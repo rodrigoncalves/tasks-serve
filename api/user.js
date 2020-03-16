590cbec7a421bcdec91237f4a46909272a4c719c
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2020-03-15 22:31:23 -0300

    106. Middleware de Segurança

--- api/user.js
+++ api/user.js
@@ -1,23 +1,26 @@
 const bcrypt = require('bcrypt-nodejs')
 
 module.exports = app => {
-  const getHash = (password, cb) => {
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(password, salt, null, (err, hash) => cb(hash))
-    })
-  }
+    const getHash = (password, cb) => {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(password, salt, null, (err, hash) => cb(hash))
+        })
+    }
 
-  const save = (req, res) => {
-    getHash(req.body.password, hash => {
-      const password = hash
+    const save = (req, res) => {
+        getHash(req.body.password, hash => {
+            const password = hash
 
-      app
-        .db('users')
-        .insert({ name: req.body.name, email: req.body.email, password })
-        .then(_ => res.status(204).send())
-        .catch(err => res.status(400).json(err))
-    })
-  }
+            app.db('users')
+                .insert({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password
+                })
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(400).json(err))
+        })
+    }
 
-  return { save }
+    return { save }
 }
--- config/middlewares.js
+++ config/middlewares.js
@@ -2,10 +2,10 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 module.exports = app => {
-  app.use(bodyParser.json())
-  app.use(
-    cors({
-      origin: '*'
-    })
-  )
+    app.use(bodyParser.json())
+    app.use(
+        cors({
+            origin: '*'
+        })
+    )
 }
